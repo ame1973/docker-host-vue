d5b192709447eb95c765a543ad49037297a5dc28
HuanXiang <qq418610675@gmail.com>
2021-09-07 11:41:08 +0800

    Update depoly.sh

--- depoly.sh
+++ depoly.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ "$1" == "" && "$2" == ""]; then
+    echo "Missing PROJECT_NAME or PROJECT_DOMAIN.com"
+fi
+
 cp docker-compose.example docker-compose.yml
 
 sed -i "s/YOUR_PROJECT_NAME/$1/g" docker-compose.yml
