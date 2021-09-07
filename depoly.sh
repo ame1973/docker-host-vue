ee376a3a2e2b11915e30275d7f8ad61961283e76
HuanXiang <qq418610675@gmail.com>
2021-09-07 11:46:46 +0800

    Update depoly.sh

--- depoly.sh
+++ depoly.sh
@@ -2,14 +2,22 @@
 
 if [ -z "$1" ]
 then
-    echo "Please input PROJECT_NAME arguments!"
+    echo "[ERROR] Please input PROJECT_NAME arguments!"
     exit
 fi
 
 if [ -z "$2" ]
 then
-    echo "Please input PROJECT_DOMAIN arguments!"
+    echo "[ERROR] Please input PROJECT_DOMAIN arguments!"
     exit
+fi
+
+DIR="./src/"
+if [ -d "$DIR" ]; then
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "[ERROR] ${DIR} not found. Can not continue."
+  exit 1
 fi
 
 cp docker-compose.example docker-compose.yml
