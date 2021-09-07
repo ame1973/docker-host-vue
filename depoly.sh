24e4e8d877ed02ee6b31e85331d35a7c09f6c85a
HuanXiang <qq418610675@gmail.com>
2021-09-07 11:48:31 +0800

    Update depoly.sh

--- depoly.sh
+++ depoly.sh
@@ -12,13 +12,6 @@ then
     exit
 fi
 
-DIR="./src/"
-if [ -d "$DIR" ]; then
-else
-  ###  Control will jump here if $DIR does NOT exists ###
-  echo "[ERROR] ${DIR} not found. Can not continue."
-  exit 1
-fi
 
 cp docker-compose.example docker-compose.yml
 
@@ -26,3 +19,14 @@ sed -i "s/YOUR_PROJECT_NAME/$1/g" docker-compose.yml
 sed -i "s/YOUR_PROJECT_DOMAIN.com/$2/g" docker-compose.yml
 
 sed -i "s/YOUR_PROJECT_DOMAIN.com/$2/g" docker-compose.yml
+
+DIR="./src/"
+
+if [ -d "$DIR" ]; then
+  ### Take action if $DIR exists ###
+  echo "[INFO] docker composer runing. ${DIR}..."
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "[ERROR] ${DIR} not found. Can not continue."
+  exit 1
+fi
